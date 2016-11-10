833543a70ee900478a3606ce0aac1e2d11138d05
duan <18720935934@163.com>
2016-11-10 21:57:16 +0800

    first

---
+++ main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************
+ *       Filename:  main.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年11月10日 21时52分21秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello");
+	return 0;
+}
+
+
